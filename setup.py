51bddeb322a3abb2c493a3d3d3d0136590e49068
nathants <me@nathants.com>
2018-11-01 14:06:45 -0700

    py37 not strictly required

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.0.1",
     license='mit',
     name='py-pool',
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     author='nathan todd-stone',
     author_email='me@nathants.com',
     url='http://github.com/nathants/py-pool',
